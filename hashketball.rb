833220f530a089ec1026a8e98b7d5dea2a36959b
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-07 20:22:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,6 +119,12 @@ def game_hash
     }
 end
 
+def num_points_scored(player_name)
+find_player(player_name)[:points]
+end
+
+
+
 def good_practices
   game_hash.each do |location, team_data|
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
